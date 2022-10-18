cebdfe373fd6b4b06f87a3b17d4bf191d3da13fd
shypilovd1986 <shypilovd@gmail.com>
2022-10-18 23:02:59 +0300

    add form, flashed messages to registration page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, flash
 
 @app.route('/index')
 @app.route('/home')
@@ -8,14 +8,20 @@ def index():
     return render_template('index.html', index=True)
 
 listTimetable={'Monday': ['Algebra', 'Chemistry', 'Literature', 'Music'],
-           'Thusday': ['Technology ', 'Algebra', 'History', 'Music']}
+           'Thusday': ['Technology ', 'Algebra', 'History', 'Music']
+               }
 @app.route('/timetable')
 def timetable():
     return render_template('timetable.html', listTimetable=listTimetable, timetable=True)
 
-@app.route('/register')
+@app.route('/register', methods=["POST", "GET"])
 def register():
-    return render_template('register.html', register=True)\
+    if request.method == 'POST':
+        if len(request.form['firstName']) < 3 or len(request.form['lastName']) < 3:
+            flash('Слишком короткое имя или фамилия', category='error')
+        else:
+            flash('Регистрация прошла успешно', category='success')
+    return render_template('register.html', register=True)
 
 @app.route('/login')
 def login():
